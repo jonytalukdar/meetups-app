98b632d0a9ecfc62c268d0a1d661127a027f1d04
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 13:30:20 +0600

    added links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Switch, Route } from 'react-router';
+import MainNavigation from './components/layout/MainNavigation';
 import AllMeetups from './pages/AllMeetups';
 import Favorites from './pages/Favorites';
 import NewMeetup from './pages/NewMeetup';
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={AllMeetups} />
-      <Route path="/newMeetup" component={NewMeetup} />
-      <Route path="/favorites" component={Favorites} />
-    </Switch>
+    <>
+      <MainNavigation />
+      <Switch>
+        <Route exact path="/" component={AllMeetups} />
+        <Route path="/newMeetup" component={NewMeetup} />
+        <Route path="/favorites" component={Favorites} />
+      </Switch>
+    </>
   );
 }
 
