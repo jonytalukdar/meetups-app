2bb6bb170e8647225ff53671adfed7420bf1598a
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 17:27:32 +0600

    completed post request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,23 @@ import React from 'react';
 import MeetupForm from '../components/meetups/MeetupForm';
 
 const NewMeetup = () => {
+  const addMeetupHandler = (data) => {
+    fetch(
+      'https://portfolio-5220b-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  };
+
   return (
     <section>
       <h2>Add New Meetup</h2>
-      <MeetupForm />
+      <MeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
