eed8ce0952e2982002951c13a7c73c2d5cc36d54
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 18:58:39 +0600

    add fetching meetups

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_DATA = [
@@ -22,10 +23,52 @@ const DUMMY_DATA = [
 ];
 
 const AllMeetups = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [meetups, setMeetups] = useState([]);
+
+  const fetchMeetups = async () => {
+    setIsLoading(true);
+    const response = await fetch(
+      'https://portfolio-5220b-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+    );
+
+    const data = await response.json();
+
+    const newData = [];
+    // convert object to arry
+    for (const key in data) {
+      const meetup = {
+        id: key,
+        ...data[key],
+      };
+
+      newData.push(meetup);
+    }
+
+    setMeetups(newData);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMeetups();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section style={{ textAlign: 'center' }}>
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      {meetups.length === 0 ? (
+        <h2>No Meetup Found</h2>
+      ) : (
+        <MeetupList meetups={meetups} />
+      )}
     </section>
   );
 };
