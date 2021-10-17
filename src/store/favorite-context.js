782ba4fe971ba17629c6a32cd50154d974c84196
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 21:21:35 +0600

    added logic for favorite

--- src/store/favorite-context.js
+++ src/store/favorite-context.js
@@ -1,12 +1,41 @@
 import React, { createContext } from 'react';
+import { useState } from 'react/cjs/react.development';
 
 export const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
+  addFavoriteHandler: () => {},
+  removeFavoriteHandler: () => {},
+  isItemFavoriteHandler: () => {},
 });
 
 const FavoritesContextProvider = (props) => {
-  const context = {};
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const addFavoriteHandler = (favoriteMeetup) => {
+    setUserFavorites((prevsState) => {
+      return { ...prevsState, favoriteMeetup };
+    });
+  };
+
+  const removeFavoriteHandler = (id) => {
+    setUserFavorites((prevState) => {
+      return prevState.filter((meetup) => meetup.id !== id);
+    });
+  };
+
+  const isItemFavoriteHandler = (id) => {
+    return userFavorites.some((meetup) => meetup.id === id);
+  };
+
+  const context = {
+    fovorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavoriteHandler,
+    removeFavoriteHandler,
+    isItemFavoriteHandler,
+  };
+
   return (
     <FavoritesContext.Provider value={context}>
       {props.children}
