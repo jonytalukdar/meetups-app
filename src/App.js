6ee24d4291701e156d739695dcda842ebfc48428
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 10:55:32 +0600

    working with style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <h2>hello world</h2>;
+  return (
+    <div>
+      <h2>My Todos</h2>
+      <div className="card">
+        <h3>Title</h3>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
