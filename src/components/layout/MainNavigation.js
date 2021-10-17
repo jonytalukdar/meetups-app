ab155f26307c72c78df27bb7c1de0c4f72f8ebbd
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 15:27:33 +0600

    added Navlink

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link to="/">All Meetups</Link>
+            <NavLink exact to="/" activeClassName={classes.active}>
+              All Meetups
+            </NavLink>
           </li>
           <li>
-            <Link to="/newMeetup">New Meetup</Link>
+            <NavLink to="/newMeetup" activeClassName={classes.active}>
+              New Meetup
+            </NavLink>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <NavLink to="/favorites" activeClassName={classes.active}>
+              Favorites
+            </NavLink>
           </li>
         </ul>
       </nav>
