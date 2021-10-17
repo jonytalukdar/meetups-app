8d6b87b21aef7a3410a21c7948c2b6657288fb16
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 13:07:30 +0600

    start for meetup project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
     <div>
-      <h2>My Todos</h2>
-      <Todo title="Learing React" />
-      <Todo title="Mastering React" />
-      <Todo title="React is Awesome" />
+      <h2>hello</h2>
     </div>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,5 +0,0 @@
-const Backdrop = (props) => {
-  return <div className="backdrop" onClick={props.onClose} />;
-};
-
-export default Backdrop;
