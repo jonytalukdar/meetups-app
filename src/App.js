e269560977bdf3c01bed25a129da4156f19c7505
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 11:12:08 +0600

    work with props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h2>My Todos</h2>
-      <Todo />
+      <Todo title="Learing React" />
+      <Todo title="Mastering React" />
+      <Todo title="React is Awesome" />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
-const Todo = () => {
+const Todo = (props) => {
   return (
     <div className="card">
-      <h3>Title</h3>
+      <h3>{props.title}</h3>
       <div className="actions">
         <button className="btn">Delete</button>
       </div>
