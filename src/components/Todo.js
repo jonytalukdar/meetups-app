0027ce471aab8445715a131f78f8ef9f9c514fb0
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-17 11:42:59 +0600

    adde events handler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,16 @@
 const Todo = (props) => {
+  const deleteHandler = () => {
+    console.log('clicked');
+    console.log(props.title);
+  };
+
   return (
     <div className="card">
       <h3>{props.title}</h3>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
