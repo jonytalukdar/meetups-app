c238971a94710612f1655d619a58b165dac98a0c
jonytalukdar <jonytalukdar30@gmail.com>
2021-10-18 21:45:46 +0600

    added favorite to database

--- src/store/favorite-context.js
+++ src/store/favorite-context.js
@@ -1,10 +1,10 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
 
 export const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
-  addFavorite: (favoriteMeetip) => {},
+  addFavorite: (favoriteMeetup) => {},
   removeFavorite: (id) => {},
   isFavorite: (id) => {},
 });
@@ -12,13 +12,58 @@ export const FavoritesContext = createContext({
 const FavoritesContextProvider = (props) => {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  console.log(userFavorites);
+
   const addFavoriteHandler = (favoriteMeetup) => {
-    setUserFavorites((prevsState) => {
-      return [...prevsState, favoriteMeetup];
+    fetch(
+      'https://portfolio-5220b-default-rtdb.asia-southeast1.firebasedatabase.app/favorites.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(favoriteMeetup || null),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    setUserFavorites((prevState) => {
+      return [...prevState, favoriteMeetup];
     });
   };
 
+  const fetchFavorites = async () => {
+    const response = await fetch(
+      'https://portfolio-5220b-default-rtdb.asia-southeast1.firebasedatabase.app/favorites.json'
+    );
+
+    const data = await response.json();
+
+    const newData = [];
+    // convert object to arry
+    for (const key in data) {
+      const favorites = {
+        id: key,
+        ...data[key],
+      };
+
+      newData.push(favorites);
+    }
+
+    setUserFavorites(newData);
+  };
+
+  useEffect(() => {
+    fetchFavorites();
+  }, []);
+
   const removeFavoriteHandler = (id) => {
+    fetch(
+      `https://portfolio-5220b-default-rtdb.asia-southeast1.firebasedatabase.app/${id}.json`,
+      {
+        method: 'DELETE',
+      }
+    );
+
     setUserFavorites((prevState) => {
       return prevState.filter((meetup) => meetup.id !== id);
     });
